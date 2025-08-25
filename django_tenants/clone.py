94e626f06506b053a36d5cff07e33b518bf8545f
Alexander Todorov <atodorov@otb.bg>
2025-08-25 13:58:26 +0300

    Revert "clone: allow setting up the clone mode (DATA, NODATA)"
    This reverts commit 55fdf33c9e97b2d51803c3bd1983a9a7ccb6d436.

--- django_tenants/clone.py
+++ django_tenants/clone.py
@@ -3799,9 +3799,7 @@ class CloneSchema:
         cursor.execute(CLONE_SCHEMA_FUNCTION.format(db_user=db_user))
         cursor.close()
 
-    def clone_schema(
-        self, base_schema_name, new_schema_name, clone_mode="DATA", set_connection=True
-    ):
+    def clone_schema(self, base_schema_name, new_schema_name, set_connection=True):
         """
         Creates a new schema `new_schema_name` as a clone of an existing schema
         `old_schema_name`.
@@ -3820,13 +3818,8 @@ class CloneSchema:
         if schema_exists(new_schema_name):
             raise ValidationError("New schema name already exists")
 
-        sql = "SELECT clone_schema(%(base_schema)s, %(new_schema)s, %(clone_mode)s)"
+        sql = "SELECT clone_schema(%(base_schema)s, %(new_schema)s, 'DATA')"
         cursor.execute(
-            sql,
-            {
-                "base_schema": base_schema_name,
-                "new_schema": new_schema_name,
-                "clone_mode": clone_mode,
-            },
+            sql, {"base_schema": base_schema_name, "new_schema": new_schema_name}
         )
         cursor.close()
